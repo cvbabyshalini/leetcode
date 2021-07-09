110020f1b0e950e2bc2f5bb539618e40587a8bb1
cvbabyshalini <84315563+cvbabyshalini@users.noreply.github.com>
2021-07-09 15:12:11 +0530

    Time: 0 ms (100.00%), Space: 37.7 MB (6.89%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,18 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        int count = 0,k=0;
-        
-        if (n == 1) {
-            return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++){
+            dp[i] += dp[i-1];
+            if(i-2 >=0)
+                dp[i] += dp[i-2];
         }
-        int first = 1;
-        int second = 2;
-        for (int i = 3; i <= n; i++) {
-            int third = first + second;
-            first = second;
-            second = third;
-        }
-        return second;
-         
+        return dp[n];
     }
 }
