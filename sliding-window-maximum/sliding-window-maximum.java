0b4efdb1ab1fcb49fa2b41bd300bdbae9a570fe7
cvbabyshalini <84315563+cvbabyshalini@users.noreply.github.com>
2021-09-20 10:52:28 +0530

    Time: 53 ms (34.99%), Space: 132.7 MB (11.88%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window-maximum/sliding-window-maximum.java
@@ -2,35 +2,31 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int[] arr = new int[nums.length-k+1];
         Deque<Integer> dq = new ArrayDeque<>();
-        
-        for(int i = 0; i < k; i++){
+        for(int i = 0 ; i < k ; i++){
             
             // if last ele in deque is less than nums[i] then remove
-            while(dq.size() > 0 && nums[dq.getLast()]<nums[i])
+            while(dq.size() > 0 && nums[dq.getLast()] < nums[i])
                 dq.removeLast();
             
-           dq.addLast(i);
+            dq.addLast(i);
         }
-       arr[0] = nums[dq.getFirst()];
+        arr[0] = nums[dq.getFirst()];
         int m = 1;
-        
-        for(int i = k; i < nums.length; i++){
+        for(int i = k ; i < nums.length ; i++){
             
             // if last ele in deque is less than nums[i] then remove
-            while(dq.size() > 0 && nums[dq.getLast()]<nums[i])
+            while(dq.size() > 0 && nums[dq.getLast()] < nums[i])
                 dq.removeLast();
             
             dq.addLast(i);
             
             // if 1st ele in deque is not in the curr slide then remove
-            if(dq.getFirst() <= i-k )
+            if(dq.getFirst() <= i-k)
                 dq.removeFirst();
             
             arr[m] = nums[dq.getFirst()];
             m++;
         }
-        
         return arr;
-        
     }
 }
