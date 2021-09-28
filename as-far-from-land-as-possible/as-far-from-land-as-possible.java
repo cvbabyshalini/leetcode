cc15b25b1c0f6a48c287dbb3987804a1e2eab460
cvbabyshalini <84315563+cvbabyshalini@users.noreply.github.com>
2021-09-28 19:12:12 +0530

    Time: 13 ms (61.84%), Space: 39.9 MB (76.42%) - LeetHub

---
+++ as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -0,0 +1,54 @@
+class Solution {
+    static int[] xdir = {-1, 0, 1, 0};
+    static int[] ydir = {0, -1, 0, 1};
+    
+    public static class Pair01{
+        int i ;
+        int j ;
+        Pair01(int i , int j ){
+            this.i = i;
+            this.j = j;
+        }
+    }
+    public int maxDistance(int[][] mat) {
+        Queue<Pair01> qu = new ArrayDeque<>();
+        boolean[][] vis = new boolean[mat.length][mat[0].length];
+        
+        for(int i = 0 ; i < mat.length ; i++){
+            for(int j = 0 ; j < mat[0].length ; j++){
+                if(mat[i][j] == 1){
+                    qu.add(new Pair01(i, j));
+                    vis[i][j] = true;
+                }
+            }
+        }
+        if(qu.size() == 0 || qu.size() == mat.length * mat[0].length)
+            return -1;
+        
+        int level = -1;
+        while(qu.size() > 0){
+            int size = qu.size();
+            level++;
+            while(size-- > 0){
+                Pair01 rem = qu.remove();
+                mat[rem.i][rem.j] = level ;
+                for(int d = 0 ; d < 4 ; d++){
+                    int r = rem.i + xdir[d];
+                    int c = rem.j + ydir[d];
+                    
+                    if(r >= 0 && r < mat.length && c >= 0 && c < mat[0].length && vis[r][c] == false){
+                        qu.add(new Pair01(r, c));
+                        vis[r][c] = true;
+                    }
+                }
+            }
+        }
+        int max = 0 ;
+        for(int i = 0 ; i < mat.length ; i++){
+            for(int j = 0 ; j < mat[0].length ; j++){
+                max = Math.max(mat[i][j] , max);
+            }
+        }
+        return max;
+    }
+}
