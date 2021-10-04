0e44a641cc191286e59c787a7e8af8c8fe99ad47
cvbabyshalini <84315563+cvbabyshalini@users.noreply.github.com>
2021-10-04 14:49:03 +0530

    Time: 1 ms (53.57%), Space: 47.9 MB (21.58%) - LeetHub

--- reshape-the-matrix/reshape-the-matrix.java
+++ reshape-the-matrix/reshape-the-matrix.java
@@ -3,19 +3,13 @@ class Solution {
         if(mat.length * mat[0].length != r * c)
                 return mat;
         int[] helper = new int[mat.length * mat[0].length];
-        int k = 0 ;
-        for(int i = 0 ; i < mat.length ;i++){
-            for(int j = 0 ; j < mat[0].length ; j++){
-                helper[k++] = mat[i][j];
-            }
+        for(int i = 0 ; i < helper.length ;i++){
+            helper[i] = mat[i/mat[0].length][i%mat[0].length];
         }
-        k = 0 ;
-        int[][] ans = new int[r][c];
-        for(int i = 0 ; i < r ; i++){
-            for(int j = 0; j < c ; j++){
-                ans[i][j] = helper[k++];
-            }
+        int[][] res = new int[r][c];
+        for(int i = 0 ; i < helper.length ; i++){
+            res[i/c][i%c] = helper[i];
         }
-        return ans;
+        return res;
     }
 }
