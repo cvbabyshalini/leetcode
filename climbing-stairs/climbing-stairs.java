40db638f5bd4fc5ddda106487d14ddd618bf5239
cvbabyshalini <84315563+cvbabyshalini@users.noreply.github.com>
2021-10-05 22:22:07 +0530

    Time: 0 ms (100.00%), Space: 37.7 MB (14.46%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -3,10 +3,9 @@ class Solution {
         int[] dp = new int[n + 1];
         dp[0] = 1;
         for(int i = 1 ; i <= n ; i++){
-            dp[i] += dp[i - 1];
-            if(i - 2 >= 0){
-                dp[i] += dp[i - 2];
-            }
+            dp[i] += dp[i-1];
+            if(i - 2 >= 0)
+                dp[i] += dp[i-2];
         }
         return dp[n];
     }
