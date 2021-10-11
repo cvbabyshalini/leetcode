68fbee4fe4ad0e8bd521aa19b81ccc59ab2333fc
cvbabyshalini <84315563+cvbabyshalini@users.noreply.github.com>
2021-10-11 15:11:48 +0530

    Time: 4 ms (39.45%), Space: 40 MB (12.20%) - LeetHub

--- rotting-oranges/rotting-oranges.java
+++ rotting-oranges/rotting-oranges.java
@@ -1,5 +1,4 @@
 class Solution {
-    
     static int[] xdir = {-1 , 0 , 1 , 0};
     static int[] ydir = {0 , -1 , 0 , 1};
     public class OPair {
@@ -16,7 +15,6 @@ class Solution {
         Queue<OPair> q = new ArrayDeque<>();
         int orange = 0 ;
         // travel on grid and add rotten oranges in the queue and count total no. of oranges
-        
         for(int r = 0 ; r < grid.length ; r++){
             for(int c = 0 ; c < grid[0].length ; c++){
                 if(grid[r][c] == 2)
@@ -25,7 +23,6 @@ class Solution {
                     orange++;
             }
         }
-        
         int time = 0;
         while(q.size() > 0){
             OPair rem = q.remove();
